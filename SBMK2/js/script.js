d755a530b04236ccc517a0117c310038be961b27
Klaus Jensen <klausjensendk@gmail.com>
2025-10-26 16:35:39 +0100

    26-10

--- SBMK2/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggleStrike(element) {
-    if (element.style.textDecoration === "line-through") {
-element.style.textDecoration = "none";
-    } else {
-element.style.textDecoration = "line-through";
-    }
-}
